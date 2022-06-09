01795160aff2fc7177d8657f610f4f2e321d4a64
fedcserg <serhii.fedchenko@ghostdrive.com>
2022-06-09 16:10:28 +0300

    adding online install

---
+++ get-gxd.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env sh
+
+_exists() {
+  cmd="$1"
+  if [ -z "$cmd" ] ; then
+    echo "Usage: _exists cmd"
+    return 1
+  fi
+  if type command >/dev/null 2>&1 ; then
+    command -v $cmd >/dev/null 2>&1
+  else
+    type $cmd >/dev/null 2>&1
+  fi
+  ret="$?"
+  return $ret
+}
+
+if [ -z "$BRANCH" ]; then
+  BRANCH="main"
+fi
+
+if _exists curl; then
+  curl -s https://raw.githubusercontent.com/acmesh-official/acme.sh/$BRANCH/acme.sh -o docker-install.sh \
+    && sudo sh ./docker-install.sh && rm -f docker-install.sh
+elif _exists wget; then
+  wget -qO -  https://raw.githubusercontent.com/acmesh-official/acme.sh/$BRANCH/acme.sh \
+    && sudo sh ./docker-install.sh && rm -f docker-install.sh
+else
+  echo "Sorry, you must have curl or wget installed first."
+  echo "Please install either of them and try again."
+fi
