a78825f40bfdb98256926610433b419088bdaec7
fedcserg <98430186+fedcserg@users.noreply.github.com>
2022-08-08 16:55:38 +0300

    Update get-gxd-docker.sh

--- get-gxd-docker.sh
+++ get-gxd-docker.sh
@@ -49,7 +49,8 @@ if command_exists docker && [ -e /var/run/docker.sock ]; then
 else
   echo "Docker is not detected. Installing Docker..."
   curl -fsSL https://get.docker.com -o get-docker.sh
-  sudo sh get-docker.sh && sudo usermod -aG docker "$USER" && newgrp docker
+  (. ./get-docker.sh)
+  sudo usermod -aG docker "$USER" && newgrp docker
 fi
 
 #GhostCloud IPFS node spawn
